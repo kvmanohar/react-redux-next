5bd3fa3be20b5e2415f14c80131cd8520c5be9a5
Manohar Kurapati <kvmanohar@gmail.com>
2020-01-25 14:02:58 +0000

    Completed React-NEXT app setup

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const Index = () => (
+	<div>
+		<h2 className="title is-2"> Authendication with Next.js and JWT</h2>
+		<p>A proof of concept app, demonstrating the authentication of Next.js application using JWT.</p>
+	</div>
+);
+
+export default Index;
